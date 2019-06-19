f8036a320e48fb407aab80a7811a0cc898f9a0df
surpreet.mundra <surpreet.mundra@gmail.com>
2019-06-19 00:11:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,16 @@ class App < Sinatra::Base
       @team_name = params[team][:name]
       @team_motto = params[team][:motto]
 
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @team_members = params[:team][:members]
+      @team_members.each do |hero|
+        @hero_name << hero[:name]
+        @hero_power << hero[:power]
+        @hero_bio << hero[:bio]
+      end
+
       erb :team
     end
 
